7b2566586e53cc12902a1d1256ae5bb325e30e99
Andrea Toston <tostonar@gmail.com>
2018-05-28 13:55:55 -0400

    Got rid of delete button & simplified deleteGrid()

--- designs.js
+++ designs.js
@@ -1,37 +1,37 @@
 function respondToTheClick() {
-  let color = document.querySelector('#colorPicker').value;
-
-  this.style.backgroundColor = `${color}`;
-
+  let choice = color.value;
+  this.style.backgroundColor = `${choice}`;
 }
 
 function makeGrid(height, width) {
   for (var i = 0; i < height; i++) {
-    let row = document.createElement('tr');
+    const row = document.createElement('tr');
+
     for (var j = 0; j < width; j++) {
       const cell = document.createElement('td');
-      cell.addEventListener('click', respondToTheClick);
+
       row.appendChild(cell);
+      cell.addEventListener('click', respondToTheClick);
     }
-    document.querySelector('table').appendChild(row);
+    pixelCanvas.appendChild(row);
   }
 }
 
-
 // Select color input
-let color = document.querySelector('#colorPicker').value;
+const color = document.querySelector('#colorPicker');
 
+// Select design Canvas
+const pixelCanvas = document.querySelector('#pixelCanvas');
 
-// Select button
+// Select buttons
 const button = document.querySelector('input[type="submit"]');
-const deleteButton = document.querySelector('#delete');
 const clearButton = document.querySelector('#clear');
 
 // When size is submitted by the user, call makeGrid()
 button.addEventListener('click', function(evt) {
   evt.preventDefault();
   // if you want the grid to be automatically replaced with a new one each time you hit submit
-  // deleteGrid();
+  deleteGrid();
   // Select size input
   let height = document.querySelector('#inputHeight').value;
   let width = document.querySelector('#inputWidth').value;
@@ -41,18 +41,10 @@ button.addEventListener('click', function(evt) {
 
 
 // delete Grid
-const designHeading = document.querySelector('#designHeading');
-const htmlTextToAdd = '<table id="pixelCanvas"></table>';
-function deleteGrid() {
-  document.querySelector('table').remove();
-  designHeading.insertAdjacentHTML('afterend', htmlTextToAdd);
-}
 
-// hook deleteButton to deleteGrid function
-deleteButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  deleteGrid();
-});
+function deleteGrid() {
+  pixelCanvas.innerHTML = "";
+}
 
 // Clear the grid
 function clearGrid() {
